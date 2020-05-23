425f0edb2b80e04bc803decbb4e0aa08d2734d78
Daniel Tadesse <tsi@Tsis-MacBook-Pro.local>
2020-05-23 19:51:13 -0400

    Added Java Configuration

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,22 @@
+import com.pluralsight.repository.HibernateSpeakerRepostiroyImpl;
+import com.pluralsight.repository.SpeakerRepostiroy;
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService(){
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        service.setRepository(getSpeakerRepository());
+        return service;
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepostiroy getSpeakerRepository(){
+        return new HibernateSpeakerRepostiroyImpl();
+    }
+}
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -7,9 +7,13 @@ import com.pluralsight.repository.SpeakerRepostiroy;
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private SpeakerRepostiroy repository = new HibernateSpeakerRepostiroyImpl();
+    private SpeakerRepostiroy repository;
     @Override
     public List<Speaker> findAll(){
         return  repository.findAll();
+    }
+
+    public void setRepository(SpeakerRepostiroy repository) {
+        this.repository = repository;
     }
 }
